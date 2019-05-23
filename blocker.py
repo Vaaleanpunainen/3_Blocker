22bae31c0c987d15565114e463fc151db5c87541
Ewelina Backtrog <oyfg@o2.pl>
2019-05-23 16:34:20 +0200

    add second part of code

--- blocker.py
+++ blocker.py
@@ -1,7 +1,6 @@
 import time
 from datetime import datetime as dt
 
-hosts_temp = r"hosts"
 hosts_path = r"C:\Windows\System32\drivers\etc"
 redirect = "127.0.0.1"
 websites_list = ["www.facebook.com", "facebook.com"]
@@ -9,7 +8,7 @@ websites_list = ["www.facebook.com", "facebook.com"]
 while True:
     if (dt(dt.now().year, dt.now().month, dt.now().day, 10)) < dt.now() < (dt(dt.now().year, dt.now().month, dt.now().day, 15)):
         print("Working hours...")
-        with open(hosts_temp, "r+") as file:
+        with open(hosts_path, "r+") as file:
             content = file.read()
             for ws in websites_list:
                 if ws in content:
@@ -17,5 +16,12 @@ while True:
                 else:
                     file.write(redirect + " " + ws + "\n")
     else:
+        with open(hosts_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(ws in line for ws in websites_list):
+                    file.write(line)
+            file.truncate()
         print("Free time...")
     time.sleep(5)
